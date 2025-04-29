f680f4687de390da96bf69d3078bdc38b90573a7
samkits <samkitsalot0@gmail.com>
2025-04-29 13:14:00 +0530

    Create a game flow in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("--------- Start ------------");
+
+        Player player1 = new Player(0, "Alice", PieceType.CIRCLE);
+        Player player2 = new Player(1, "Bob", PieceType.CROSS);
+        GameBoard gameBoard = new GameBoard(3,3);
+        gameBoard.addPlayer(player1);
+        gameBoard.addPlayer(player2);
+        gameBoard.startGame(player1);
+
+        System.out.println("--------- End -------------");
     }
 }
